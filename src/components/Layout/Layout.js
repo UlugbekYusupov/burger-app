bf14d79bdcc5097ce532a3009ebaa5db6a155741
Mirzoulugbek Yusupov <mirzoulugbek@Mirzoulugbeks-iMac.local>
2021-02-22 15:46:27 +0900

    Outputting the ingredients dynamically and calculating the price of ingredients

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Aux from '../../hoc/Aux';
-import classes from './Layout.css'
+import styles from './Layout.module.css'
+import Burger from '../Burger/Burger'
 
 function layout(props) {
     return (
         <Aux>
             <div>ToolBar, SideDrawer, Backdrop</div>
-            <main className={classes.Content}>
+            <main className={styles.Content}>
                 {props.children}
             </main>
         </Aux>
